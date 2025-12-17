c8487c61862c9fea34f92b5e273b5f2e0c9dd256
Konsento <95830789+kons10@users.noreply.github.com>
2025-12-17 14:27:48 +0900

    Add Material Web typography styles to mwc.js

---
+++ assets/js/mwc.js
@@ -0,0 +1,4 @@
+import 'https://esm.run/@material/web/all.js';
+import {styles as typescaleStyles} from 'https://esm.run/@material/web/typography/md-typescale-styles.js';
+
+document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
